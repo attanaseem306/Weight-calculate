5ebafa868ffc7485fcec8b6740be4466f686fcbf
attanaseem306 <attanaseem306@gmail.com>
2023-07-04 03:05:05 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function Calculate() {
+    var first = document.getElementById("first").value;
+    var multiply = first * 2.20462.toFixed(3);
+    var second = document.getElementById("second").value = multiply;
+    var multiply2 = first * 35.27.toFixed(3);
+    var Ounces = document.getElementById("third").value = multiply2;
+}
+
+
+
